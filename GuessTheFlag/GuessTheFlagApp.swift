7e6c4ce867f93721c3fedf9fab3100219bec7c8d
dev-maryamkhursheed <maryamkhursheed0204@gmail.com>
2025-10-02 16:37:42 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Maryam Khursheed on 2025-10-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
